91830f9315c171477b303b6fd4791d656ee1f2cc
James DeFelice <james.defelice@gmail.com>
2015-05-11 22:04:11 +0000

    fixes #154

--- records/labels/dns952.go
+++ records/labels/dns952.go
@@ -7,13 +7,13 @@ var dns952table []int32
 func init() {
 	const tolower = int32('a' - 'A')
 	dns952table = make([]int32, 256, 256)
-	for i := int32('A'); i < int32('Z'); i++ {
+	for i := int32('A'); i <= int32('Z'); i++ {
 		dns952table[i] = i + tolower
 	}
-	for i := int32('a'); i < int32('z'); i++ {
+	for i := int32('a'); i <= int32('z'); i++ {
 		dns952table[i] = i
 	}
-	for i := int32('0'); i < int32('9'); i++ {
+	for i := int32('0'); i <= int32('9'); i++ {
 		dns952table[i] = -i
 	}
 	dns952table[int32('-')] = -int32('-')
@@ -23,7 +23,7 @@ func init() {
 
 // mangle the given name to be compliant as a DNS952 name "component".
 // the returned result should match the regexp:
-//    ^[a-z]([-a-z0-9]*[a-z0-9])?$
+//    ^[a-z]([-a-z0-9]*?[a-z0-9])?$
 // returns "" if the name cannot be mangled.
 func AsDNS952(name string) string {
 	if name == "" {
