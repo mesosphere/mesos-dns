32ab8165450f55cf8d1219e44c2e5eb059e17924
Tomás Senart <tomas@mesosphere.io>
2015-09-05 10:48:53 +0200

    Revert "Allow hostnames for delegate resolvers"
    This reverts commit 5f01360602c5f87b85c3e2a82847d4ab8ce6ef0a.

--- records/validation.go
+++ records/validation.go
@@ -32,17 +32,24 @@ func validateMasters(ms []string) error {
 	return nil
 }
 
-// validateResolvers errors if there are duplicate resolvers, otherwise returns nil.
+// validateResolvers checks that each resolver in the list is a properly formatted IP address.
+// duplicate resolvers in the list are not allowed.
+// returns nil if the resolver list is empty, or else all resolvers in the list are valid.
 func validateResolvers(rs []string) error {
 	if len(rs) == 0 {
 		return nil
 	}
 	ips := make(map[string]struct{}, len(rs))
 	for _, r := range rs {
-		if _, found := ips[r]; found {
+		ip := net.ParseIP(r)
+		if ip == nil {
+			return fmt.Errorf("illegal IP specified for resolver %q", r)
+		}
+		ipstr := ip.String()
+		if _, found := ips[ipstr]; found {
 			return fmt.Errorf("duplicate resolver IP specified: %v", r)
 		}
-		ips[r] = struct{}{}
+		ips[ipstr] = struct{}{}
 	}
 	return nil
 }
