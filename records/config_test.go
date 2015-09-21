b9109640f86b58870f982b7b59dfeeb126761fb5
Tomás Senart <tomas@mesosphere.io>
2015-09-21 12:24:23 +0200

    Don't Fatal in TestNewConfigValidates

--- records/config_test.go
+++ records/config_test.go
@@ -19,23 +19,23 @@ func TestNewConfigValidates(t *testing.T) {
 	c := NewConfig()
 	err := validateIPSources(c.IPSources)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	err = validateResolvers(c.Resolvers)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	err = validateMasters(c.Masters)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	err = validateEnabledServices(&c)
 	if err == nil {
-		t.Fatal("expected error because no masters and no zk servers are configured by default")
+		t.Error("expected error because no masters and no zk servers are configured by default")
 	}
 	c.Zk = "foo"
 	err = validateEnabledServices(&c)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 }
